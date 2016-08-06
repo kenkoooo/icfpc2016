be4d8d021b6d5eefc8ec020eb6b1b0113aaf667f
kenkoooo <kenkou.n@gmail.com>
2016-08-06 13:41:38 +0900

    ビジュアライザ改造した

--- visualizer/InputVisualizer.py
+++ visualizer/InputVisualizer.py
@@ -1,13 +1,14 @@
 import argparse
+import json
 import os
 from fractions import Fraction
 
 import matplotlib.pyplot as plt
 
 
-def plot(polygons, skeleton):
-    plt.xlim([-0.5, 1.5])
-    plt.ylim([-0.5, 1.5])
+def plot(polygons, skeleton, center):
+    plt.xlim([-2.0, 2.0])
+    plt.ylim([-2.0, 2.0])
 
     # 縦横比を揃えるおまじない
     plt.gca().set_aspect('equal', adjustable='box')
@@ -16,17 +17,13 @@ def plot(polygons, skeleton):
     plt.grid(which='major', color='black', linestyle='dashed')
 
     for polygon in polygons:
+        polygon = [[p[0] - center[0], p[1] - center[1]] for p in polygon]
         plt.gca().add_patch(plt.Polygon(polygon, alpha=0.3))
 
     for edge in skeleton:
+        edge = [[p[0] - center[0], p[1] - center[1]] for p in edge]
         line = plt.Polygon(edge, closed=None, fill=None, edgecolor='r')
         plt.gca().add_patch(line)
-
-
-def pop_input(lines):
-    if len(lines) == 0:
-        return input()
-    return lines.pop(0)
 
 
 def read_input(lines):
@@ -35,30 +32,44 @@ def read_input(lines):
     lines が空なら標準入力から入力を読みだす。
     多角形とスケルトンのリストにして返す。
     """
-    n = int(pop_input(lines))
+    center_x, center_y = 0, 0
+
+    n = int(lines.pop(0))
     polygons = []
     for i in range(n):
-        v = int(pop_input(lines))
+        v = int(lines.pop(0))
         coordinates = []
         for j in range(v):
-            xy = pop_input(lines).split(",")
+            xy = lines.pop(0).split(",")
             x = Fraction(xy[0])
             y = Fraction(xy[1])
+
+            if abs(x) > abs(center_x):
+                center_x = x
+            if abs(y) > abs(center_y):
+                center_y = y
+
             coordinates.append([x, y])
         polygons.append(coordinates)
 
-    edge_num = int(pop_input(lines))
+    edge_num = int(lines.pop(0))
     skeleton = []
     for i in range(edge_num):
-        e = pop_input(lines).split(" ")
+        e = lines.pop(0).split(" ")
         edge = []
         for xy in e:
             xy = xy.split(",")
             x = Fraction(xy[0])
             y = Fraction(xy[1])
+
+            if abs(x) > abs(center_x):
+                center_x = x
+            if abs(y) > abs(center_y):
+                center_y = y
+
             edge.append([x, y])
         skeleton.append(edge)
-    return polygons, skeleton
+    return polygons, skeleton, [center_x, center_y]
 
 
 def run(args):
@@ -72,15 +83,20 @@ def run(args):
         if os.path.exists(path):
             # 既に画像がある場合は上書きしない
             print(path + " already exists.")
-            continue
+            # continue
 
         print("Loading " + filename + " ...")
         data = f.read()
         lines = data.split("\n")
-        polygons, skeleton = read_input(lines)
-        plot(polygons, skeleton)
+        polygons, skeleton, center = read_input(lines)
+        plot(polygons, skeleton, center)
         plt.savefig(path)
         plt.close()
+
+        c = {"x": str(center[0]), "y": str(center[1])}
+        f = open(path.replace(".png", "_center.json"), "w", encoding="UTF-8")
+        json.dump(c, f)
+        f.close()
 
 
 if __name__ == '__main__':
